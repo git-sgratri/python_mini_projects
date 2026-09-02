81b650662af857f1f344341f453682ce9d1f81e5
Sagar Attri <sagarattri083@gmail.com>
2026-09-02 18:27:21 +0530

    learned how to use cmd-line args in python and implemented it

--- task_tracker_cli.py
+++ task_tracker_cli.py
@@ -1,5 +1,16 @@
 import json
+import sys
 from datetime import datetime as dt
+
+def all_args():
+    print("Add a task: add \"<description>\"")
+    print("Update a task: update <ID> \"<description>\"")
+    print("Delete a task: delete <ID>")
+    print("Mark task as in-progress: mark-in-progress <ID>")
+    print("Mark task as done: mark-done <ID>")
+    print("List tasks: list")
+    print("List tasks in-progress: list-in-progress")
+    print("List tasks done: list-done")
 
 def load_tasks():
     try:
@@ -7,27 +18,6 @@ def load_tasks():
             return json.load(file)
     except FileNotFoundError:
         return []
-
-def list_tasks(tasks):
-    print("Which tasks do u want to list: ")
-    print("1. All tasks")
-    print("2. Tasks todo(not started)")
-    print("3. Tasks that are completed")
-    print("4. Tasks in progress")
-    choice = int(input("enter your choice: "))
-    print("##########################################################")
-    match choice:
-        case 1:
-            list_all(tasks)
-        case 2:
-            list_todo(tasks)
-        case 3:
-            list_done(tasks)
-        case 4:
-            list_in_progress(tasks)
-        case _:
-            print("invalid choice")
-    print("##########################################################")
     
 def list_all(tasks):
     if tasks == []:
@@ -56,7 +46,7 @@ def list_todo(tasks):
 def list_in_progress(tasks):
     idx = 0
     for task in tasks:
-        if(task["status"]=="in progress"):
+        if(task["status"]=="in-progress"):
             idx+=1
             print(f"ID:{task["id"]} -> {task["description"]}, created at: {task["created_at"]}")
     if idx==0:
@@ -66,90 +56,125 @@ def dump_helper(tasks):
     with open("tasks.json",'w') as file:
         json.dump(tasks,file,indent=4)
 
-def add_task(tasks):
-    description = input("Task description: ")
+def add_task(tasks,description):
     if tasks==[]:
-        id=0
+        id="1"
     else:
-        id = max(task["id"] for task in tasks)+1
+        id = str(max(int(task["id"]) for task in tasks)+1)
     tasks.append({"id": id,"description": description,"status":"todo","created_at": str(dt.now()),"updated_at":str(dt.now())})
     dump_helper(tasks)
+    print(f"Task added successfully (ID:{id})")
 
-def update_task(tasks):
-    print("which task do u want to update?")
-    list_all(tasks)
-    idx = int(input("Enter task id: "))
-    print("What do u want to update?")
-    print("1. Task description")
-    print("2. Task status")
-    choice = int(input("Enter your choice: "))
-    match choice:
-        case 1:
-            for i,task in enumerate(tasks):
-                if task["id"]==idx:
-                    des = input("Enter new description: ")
-                    tasks[i]["description"] = des
-                    tasks[i]["updated_at"] = str(dt.now())
-                    dump_helper(tasks)
-                    break
-        case 2:
-             for i,task in enumerate(tasks):
-                if task["id"]==idx:
-                    print("Which status you want to assign to this task: \n1. todo\n2. in progress\n3. done")
-                    st = int(input("enter your choice: "))
-                    if 0>st>3:
-                        print("invalid choice!")
-                        return
-                    elif st==1:
-                        tasks[i]["status"] = "todo"
-                    elif st==2:
-                        tasks[i]["status"] = "in progress"
-                    else:
-                        tasks[i]["status"] = "done"
-                    tasks[i]["updated_at"] = str(dt.now())
-                    dump_helper(tasks)
-                    break
-        case _:
-            print("invalid choice!")           
-
-def delete_task(tasks):
-    list_all(tasks)
-    print("Which task you want to delete?")
-    idx = int(input("Enter task ID: "))
-    for task in tasks:
-        if task["id"]==idx:
-            tasks.remove(task)
-            dump_helper(tasks)
-            return
-    print("No such task exists!")
-
-id = 1
 def main():
-    while True:
-        print("\n********************************************************************")
-        tasks = load_tasks()
-        print("Welcome to my task manger! Select your choice")
-        print("1. Add a task.")
-        print("2. Update a task.")
-        print("3. Delete a task.")
-        print("4. List tasks.")
-        print("5. exit")
-        print("*********************************************************************\n")
-        choice = int(input("enter your choice of operation: "))
-        
-        match choice:
-            case 1:
-                add_task(tasks)
-            case 2:
-                update_task(tasks)
-            case 3:
-                delete_task(tasks)
-            case 4:
-                list_tasks(tasks)
-            case 5:
-                break
+    tasks = load_tasks()
+    n = len(sys.argv)
+    if n < 2:
+        print("No arguements passed")
+    if n > 1:
+        operation = sys.argv[1]
+        match operation:
+            case "add":     # add a task to json file
+                if n==3:
+                    add_task(tasks,str(sys.argv[2]))
+                else:
+                    print("Error: Expected task description <str>")
+                    print("Try:\n\tadd \"<description>\"")
+
+            case "update":  #update a task description
+                if n>2:
+                    x = True
+                    for task in tasks:
+                        if task["id"] == sys.argv[2]:
+                            x = False
+                            if n==4:
+                                task["description"] = sys.argv[3]
+                                task["updated_at"] = str(dt.now())
+                                dump_helper(tasks)
+                            else:
+                                print("Error: Expected new description <str> for update")
+                                print("Try:\n\tupdate <ID> \"<description>\"")
+                            break
+                    if x:
+                        print("Error: No such task ID is present")
+                else:
+                    print("Error: Invalid arguements, expected task ID and description for update")
+                    print("Try:\n\tupdate <ID> \"<description>\"")
+
+            case "delete":  #delete a task completely
+                if n==3:
+                    x = True
+                    for task in tasks:
+                        if task["id"] == sys.argv[2]:
+                            x = False
+                            tasks.remove(task)
+                            dump_helper(tasks)
+                    if x:
+                        print("Error: No such task ID exists")
+                else:
+                    print("Error: Invalide arguements, expected task ID to delete")
+                    print("Try:\n\tdelete <ID>")
+
+            case "mark-in-progress":    # Change task status to in-progress
+                if n==3:
+                    x = True
+                    for task in tasks:
+                        if task["id"] == sys.argv[2]:
+                            x = False
+                            task["status"] = "in-progress"
+                            dump_helper(tasks)
+                    if x:
+                        print("Error: No such task ID exists")
+                        print("Try:\n\tmark-in-progress <ID>")
+                else:
+                    print("Error: Invalide arguements, expected task ID for updating status")
+                    print("Try:\n\tmark-in-progress <ID>")
+
+            case "mark-done":   # change task status to done
+                if n==3:
+                    x = True
+                    for task in tasks:
+                        if task["id"] == sys.argv[2]:
+                            x = False
+                            task["status"] = "done"
+                            dump_helper(tasks)
+                    if x:
+                        print("Error: No such task ID exists")
+                        print("Try:\n\tmark-done <ID>")
+                else:
+                    print("Error: Invalide arguements, expected task ID for updating status")
+                    print("Try:\n\tmark-done <ID>")
+
+            case "list":    # list all tasks
+                if n==2:
+                    list_all(tasks)
+                else:
+                    print("Error: Invalid no. arguements")
+                    print("Try:\n\tlist")
+
+            case "list-todo":   # list all todo tasks
+                if n==2:
+                    list_todo(tasks)
+                else:
+                    print("Error: Invalid no. of arguements")
+                    print("Try:\n\tlist-todo")
+
+            case "list-in-progress":    # list all tasks in-progress
+                if n==2:
+                    list_in_progress(tasks)
+                else:
+                    print("Error:Invalid no. of arguements")
+                    print("Try:\n\tlist-in-progress")
+
+            case "list-done":   # list all tasks done
+                if n==2:
+                    list_done(tasks)
+                else:
+                    print("Error: Invalid no. of arguements")
+                    print("Try:\n\tlist-done")
+                    
             case _:
-                print("invalid choice!")
+                print("Error: Invalid arguements")
+                all_args()
 
 if __name__ == "__main__":
     main()
